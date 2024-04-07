ca5de316d19745020c89683196248a85dc83a570
Rishabh <myselfrish@gmail.com>
2024-04-07 14:01:32 +0530

    initial commit

---
+++ nft-collection/hardhat.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const { API_URL_KEY, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: API_URL_KEY,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
+  solidity : "0.8.20",
+};
+
